c72cb4bc15e5aa797a01779a28a6d0c9c3a24a50
mohammadhm180 <mohammadhm180@gmail.com>
2023-05-28 23:40:43 +0330

    updating database and vote

--- Tweeter/src/Vote.java
+++ Tweeter/src/Vote.java
@@ -1,12 +1,9 @@
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.UUID;
 
-public class Vote {
-    private String voteID;
-    private String question;
-    private String ownerUsername;
-    private LocalDateTime voteDate;
+public class Vote extends Tweet{
     private String option1;
     private String option2;
     private String option3;
@@ -16,49 +13,29 @@ public class Vote {
     private int option3Count;
     private int option4Count;
 
-    public Vote(String ownerUsername,String question ,LocalDateTime voteDate, String option1, String option2, String option3, String option4) {
-        voteID=UUID.randomUUID().toString();
-        this.question=question;
-        this.ownerUsername = ownerUsername;
-        this.voteDate = voteDate;
+
+    public Vote(String text, byte[] photo, byte[] video, LocalDateTime tweetDate, String authorUsername, ArrayList<String> hashtag, String option1, String option2, String option3, String option4) {
+        super(text, photo, video, tweetDate, authorUsername, hashtag);
         this.option1 = option1;
         this.option2 = option2;
         this.option3 = option3;
         this.option4 = option4;
-        option1Count=0;
-        option2Count=0;
-        option3Count=0;
-        option4Count=0;
+        this.option1Count = 0;
+        this.option2Count = 0;
+        this.option3Count = 0;
+        this.option4Count = 0;
     }
-    public Vote(String ownerUsername,String question ,LocalDateTime voteDate, String option1, String option2, String option3, String option4,int option1Count,int option2Count,int option3Count,int option4Count) {
-        voteID=UUID.randomUUID().toString();
-        this.question=question;
-        this.ownerUsername = ownerUsername;
-        this.voteDate = voteDate;
+
+    public Vote(String text, byte[] photo, byte[] video, LocalDateTime tweetDate, String authorUsername, ArrayList<String> hashtag, int replyCount, int retweetCount, int likeCount, String option1, String option2, String option3, String option4, int option1Count, int option2Count, int option3Count, int option4Count) {
+        super(text, photo, video, tweetDate, authorUsername, hashtag, replyCount, retweetCount, likeCount);
         this.option1 = option1;
         this.option2 = option2;
         this.option3 = option3;
         this.option4 = option4;
-        this.option1Count=option1Count;
-        this.option2Count=option2Count;
-        this.option3Count=option3Count;
-        this.option4Count=option4Count;
-    }
-
-    public String getVoteID() {
-        return voteID;
-    }
-
-    public String getQuestion() {
-        return question;
-    }
-
-    public String getOwnerUsername() {
-        return ownerUsername;
-    }
-
-    public LocalDateTime getVoteDate() {
-        return voteDate;
+        this.option1Count = option1Count;
+        this.option2Count = option2Count;
+        this.option3Count = option3Count;
+        this.option4Count = option4Count;
     }
 
     public String getOption1() {
